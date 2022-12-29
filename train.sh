4dc390356595853ddb16fd52ada94a75ba7f8ea6
xiaominD <xiaominD>
2022-12-29 17:12:14 +0800

    train

---
+++ train.sh
@@ -0,0 +1,5 @@
+python3 humus_examples/train_humus_fastmri.py \
+--config_file humus_examples/experiments/fastmri/humus_leaderboard.yaml\
+--data_path /home/dxm/dxm/fastmri \
+--default_root_dir log \
+--gpus 2
